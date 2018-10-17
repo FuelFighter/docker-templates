9bcea1fc9a1b187a6ad36f9a7ea6d44fe2f7dcd1
Erlend Eriksen <erlend_ne@hotmail.com>
2018-10-17 17:06:39 +0200

    add x server to reodor-environment

--- reodor-environment/run_docker.sh
+++ reodor-environment/run_docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $DIR/config.sh
-docker run --net=host -it --privileged -v $(realpath $DIR/..):$WORKDIR --name $PROJECT_NAME --rm $DOCKER_REPOSITORY_NAME:latest
+xhost +local:root >/dev/null
+docker run \
+  --net=host \
+  --privileged \
+  --rm \
+  -v $(realpath $DIR/..):$WORKDIR \
+  -e DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --name $PROJECT_NAME \
+  -it $DOCKER_REPOSITORY_NAME:latest
+xhost -local:root >/dev/null
--- tests/reodor-environment-test.cpp
+++ tests/reodor-environment-test.cpp
@@ -1,17 +1,26 @@
 #include "ros/ros.h"
 #include <opencv2/opencv.hpp>
 
+#include <stdio.h>
+
 int main(int, char**)
 {
     cv::VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
+    {
+        printf("couldn't open video device\n");
         return -1;
+    }
 
     cv::Mat edges;
     for(;;)
     {
         cv::Mat frame;
         cap >> frame; // get a new frame from camera
+        cv::cvtColor(frame, edges, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(edges, edges, cv::Size(7,7), 1.5, 1.5);
+        cv::Canny(edges, edges, 0, 30, 3);
+        cv::imshow("edges", edges);
         if(cv::waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
